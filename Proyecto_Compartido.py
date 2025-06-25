5c797ef793fccc16abd0e95890ead26930690971
Julian Villanueva <andres07021@hotmail.com>
2025-06-25 17:17:04 -0500

    UPDATE: Codigo de jorge

---
+++ Proyecto_Compartido.py
@@ -0,0 +1,11 @@
+print('Rama Jorge')
+
+class Producto:
+    def __init__(self, nombre, precio):
+        self.nombre = nombre
+        self.precio = precio
+
+    def __str__(self):
+        return f"Producto: {self.nombre}, Precio: {self.precio}"
+
+print('Esto solo lo hizo Jorge')
