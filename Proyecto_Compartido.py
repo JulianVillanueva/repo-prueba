9704682685ef822066d15d154bf50bb7c008ddc3
Julian Villanueva <andres07021@hotmail.com>
2025-06-25 19:31:44 -0500

    Menu tienda -Jorge

--- Proyecto_Compartido.py
+++ Proyecto_Compartido.py
@@ -22,3 +22,32 @@ class Cliente:
         return f"Cliente: {self.nombre}, Cedula: {self.cedula}, Telefono: {self.telefono}, Correo: {self.correo}"
 
 print('Firma dev2')
+
+def menu():
+    print('Bienvenido a la tienda, seleccione una opcion:')
+    print('1. Añadir producto')
+    print('2. Ver productos')
+    print('3. Salir')
+    opcion = int(input('Opcion: '))
+    return opcion
+
+def main():
+    productos = []
+    while True:
+        opcion = menu()
+        if opcion == 1:
+            nombre = input('Ingrese el nombre del producto: ')
+            precio = float(input('Ingrese el precio del producto: '))
+            producto = Producto(nombre, precio)
+            productos.append(producto)
+        elif opcion == 2:
+            for producto in productos:
+                print(producto)
+        elif opcion == 3:
+            break
+        else:
+            print('Opcion no valida')
+    print('Gracias por visitarnos')
+
+if __name__ == '__main__':
+    main()
