12e42e2f9f7ed5abff73163db7a46096c497f80a
Julian Villanueva <andres07021@hotmail.com>
2025-06-25 19:24:20 -0500

    Nuevo algoritmo -Dev2

--- Proyecto_Compartido.py
+++ Proyecto_Compartido.py
@@ -4,8 +4,21 @@ class Producto:
     def __init__(self, nombre, precio):
         self.nombre = nombre
         self.precio = precio
+        productos = []
 
     def __str__(self):
         return f"Producto: {self.nombre}, Precio: {self.precio}"
 
 print('Esto solo lo hizo Jorge')
+
+class Cliente:
+    def __init__(self, nombre, cedula, telefono, correo):
+        self.nombre = nombre
+        self.cedula = cedula
+        self.telefono = telefono
+        self.correo = correo
+
+    def __str__(self):
+        return f"Cliente: {self.nombre}, Cedula: {self.cedula}, Telefono: {self.telefono}, Correo: {self.correo}"
+
+print('Firma dev2')
